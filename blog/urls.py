ece1026fa2af72257f8a15c02db455c9998576a6
Iskandar <iskandarjuraqulov15@gmail.com>
2024-05-11 18:54:13 +0500

    templates, views and urls connected

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import course
+from .views import  course, home ,price , review, teacher , contact
 
 urlpatterns = [
-
-    path('', view=course.as_view(), name='home_page'),
+    path('', view=home.as_view(), name='home'),
+    path('course/', view=course.as_view(), name='course'),
+    path('price/', view=price.as_view(), name='price'),
+    path('review/', view=review.as_view(), name='review'),
+    path('teacher/',view=teacher.as_view(), name='teacher'),
+    path('contact/', view=contact.as_view(), name='contact')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,40 @@ from django.views import View
 
 class home(View):
     def get(self, request):
+        return render(request=request, template_name='home.html')
+        
+         
 class course(View):
     def get(self, request):
-        context = {
-            'message': 'Hello, World!'
-        }
+        return render(request=request, template_name='course.html')
 
-        return render(request=request, template_name='ho me.html', context=context)
+    
+class price(View):
+    def get(self, request):
+        return render(request=request , template_name='price.html')    
+    
+
+class review(View):
+    def get(self, request):
+        return render(request=request , template_name='review.html')  
+    
+class teacher(View):
+    def get(self, request):
+        return render(request=request , template_name='teacher.html')    
+    
+
+class contact(View):
+    def get(self, request):
+        return render(request=request, template_name='contact.html')      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
